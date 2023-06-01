90754c1c44c8af73ee912233202bbb94c819619b
juhee kang <mnmn092631@gmail.com>
2023-06-01 17:51:28 +0900

    input 수정

--- 0601/input_quiz.py
+++ 0601/input_quiz.py
@@ -1,8 +1,5 @@
-print("사과의 개수를 입력해주세요.")
-count = int(input())
-print("사과의 가격을 입력해주세요.")
-price = int(input())
-print("부가세율을 입력해주세요.")
-vat = float(input())
+count = int(input("사과의 개수를 입력해주세요.: "))
+price = int(input("사과의 가격을 입력해주세요.: "))
+vat = float(input("부가세율을 입력해주세요.: "))
 
 print(f"총 가격은 {int(count * price * (1 + vat))}원입니다.")
