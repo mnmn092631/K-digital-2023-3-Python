8566533c56205b21b72e6bc089a333b0269e3cc9
juhee kang <mnmn092631@gmail.com>
2023-06-02 17:50:02 +0900

    문자열 연습문제 수정

--- 문자열/quiz.py
+++ 문자열/quiz.py
@@ -9,7 +9,7 @@ print(f"첫 번째 문자는 {inStr[0]}입니다.")
 # 문자열에서 처음 3문자를 출력하라
 print(inStr[:3])
 # 문자열에서 마지막 3문자를 출력하라
-print(inStr[len(inStr) - 3:len(inStr)])
+print(inStr[-3:])
 # 문자열의 문자를 거꾸로 출력하라
 print(inStr[::-1])
 # 문자열에 7번째 문자가 있으면 출력하고 없으면 '문자가 없습니다'라는 메시지를 출력하라
