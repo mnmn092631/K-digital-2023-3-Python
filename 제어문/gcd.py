4be9eaf2408052170fb355f9f53611941104c767
juhee kang <mnmn092631@gmail.com>
2023-06-02 16:56:15 +0900

    최대공약수 구하기

--- 제어문/gcd.py
+++ 제어문/gcd.py
@@ -4,8 +4,6 @@ b = int(input("두번째 양의 정수를 입력해주세요 : "))
 max = max(a, b)
 min = min(a, b)
 while min > 0:
-    remain = max % min
-    max = min
-    min = remain
+    max, min = min, max % min
 
 print(f"{a}, {b}의 최대공약수는 {max}입니다.")
