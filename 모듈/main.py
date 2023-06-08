40c19bf351752d04cf6cb5270a364d6d5ac447fc
juhee kang <mnmn092631@gmail.com>
2023-06-08 17:30:14 +0900

    모듈

---
+++ 모듈/main.py
@@ -0,0 +1,7 @@
+import mymath
+
+print("main.py를 실행한 경우 : ")
+print(f"더하기 결과 : {mymath.add(4, 3)}")
+print(f"빼기 결과 : {mymath.subtract(4, 3)}")
+print(f"곱하기 결과 : {mymath.multiply(4, 3)}")
+print(f"나누기 결과 : {mymath.divide(4, 3)}")
---
+++ 모듈/mymath.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return print("0으로 나눌 수 없습니다.")
+    return x / y
